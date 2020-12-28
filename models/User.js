5189d9ab56bdaac47234549f3d2d859ca1826f33
unknown <lssuseendharlal@gmail.com>
2020-12-28 18:10:21 +0530

    timestamps added

--- models/User.js
+++ models/User.js
@@ -1,10 +1,13 @@
 import mongoose from "mongoose";
 
-const userSchema = mongoose.Schema({
-  email: { type: String, required: true },
-  password: { type: String, required: true },
-  profile: { type: String },
-  s3Key: { type: String },
-});
+const userSchema = mongoose.Schema(
+  {
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    profile: { type: String },
+    s3Key: { type: String },
+  },
+  { timestamps: true }
+);
 
 export default mongoose.model("User", userSchema);
