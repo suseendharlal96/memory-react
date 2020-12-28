93ffdca54d8ca043874be6e722884c55632b7f3b
unknown <lssuseendharlal@gmail.com>
2020-12-28 17:48:36 +0530

    s3 to user profile

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,6 @@
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
+import AWS from 'aws-sdk'
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -41,7 +42,8 @@ export const signin = async (req, res) => {
   }
 };
 
-export const signup = async (req, res) => {
+export const signupWithoutFile = async (req, res) => {
+  console.log(req.body);
   if (req.body.email.trim() === "") {
     return res.status(400).json({ email: "Should not be empty" });
   } else {
@@ -80,5 +82,69 @@ export const signup = async (req, res) => {
   } catch (error) {
     res.status(500).json({ message: "Something went wrong" });
     console.log(error);
+  }
+};
+
+export const signup = async (req, res) => {
+  console.log(req.body);
+  console.log(req.file);
+  if (req.body.Email.trim() === "") {
+    return res.status(400).json({ email: "Should not be empty" });
+  } else {
+    const regEx = /^([0-9a-zA-Z]([-.w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-w]*[0-9a-zA-Z].)+[a-zA-Z]{2,9})$/;
+    if (!req.body.Email.trim().match(regEx)) {
+      return res.status(400).json({ email: "Enter a valid email" });
+    }
+  }
+  if (req.body.Password.trim() === "") {
+    return res.status(400).json({ password: "Should not be empty" });
+  } else if (req.body.Password.trim().length < 6) {
+    return res.status(400).json({ password: "Should be atleast 6 characters" });
+  }
+  if (req.body.confirmpassword.trim() === "") {
+    return res.status(400).json({ confirmPassword: "Should not be empty" });
+  } else if (req.body.Password.trim() !== req.body.confirmpassword.trim()) {
+    return res.status(400).json({ confirmPassword: "Password mismatch" });
+  }
+  try {
+    const oldUser = await UserModal.findOne({ email: req.body.Email });
+    if (oldUser) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+    const hashedPassword = await bcrypt.hash(req.body.Password, 12);
+    const s3FileUrl = process.env.FILE_URL;
+    const s3Bucket = new AWS.S3({
+      accessKeyId: process.env.ACCESS_KEY,
+      secretAccessKey: process.env.SECRET_KEY,
+      region: process.env.REGION,
+    });
+    const params = {
+      Bucket: process.env.BUCKET_NAME,
+      Key: req.body.Email,
+      Body: req.file.buffer,
+      ContentType: req.file.mimetype,
+      ACL: "public-read",
+    };
+    s3Bucket.upload(params, async (err, data) => {
+      if (err) {
+        res.status(500).json({ error: true, Message: err });
+      } else {
+        const result = await UserModal.create({
+          email: req.body.Email,
+          password: hashedPassword,
+          profile: s3FileUrl + params.Key,
+          s3Key: params.Key,
+        });
+        const token = jwt.sign(
+          { email: result.email, id: result.id },
+          process.env.SECRET,
+          { expiresIn: "1h" }
+        );
+        res.status(201).json({ result, token });
+      }
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(400).json(err);
   }
 };
--- models/User.js
+++ models/User.js
@@ -4,6 +4,7 @@ const userSchema = mongoose.Schema({
   email: { type: String, required: true },
   password: { type: String, required: true },
   profile: { type: String },
+  s3Key: { type: String },
 });
 
 export default mongoose.model("User", userSchema);
